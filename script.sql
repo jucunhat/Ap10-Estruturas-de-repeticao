7d60789adde79a38312f42d9f40f8c7c728de51b
Julia Cunha <juliacunhatassini@gmail.com>
2023-09-23 17:36:25 -0300

    ap10(repeticao): gerar pares entre 1 e 100

--- script.sql
+++ script.sql
@@ -1,8 +1,25 @@
--- Função de geração de aleatórios
-CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
-INT) RETURNS INT AS
+-- Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+DO
 $$
+DECLARE
+	contador INT := 0;
 BEGIN
-RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+	LOOP
+		contador := contador + 1;
+		EXIT WHEN contador > 100;
+		IF contador % 2 = 1 THEN
+			CONTINUE;
+		END IF;
+		RAISE NOTICE '%', contador;
+	END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$$
+
+-- -- Função de geração de aleatórios
+-- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+-- INT) RETURNS INT AS
+-- $$
+-- BEGIN
+-- RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+-- END;
+-- $$ LANGUAGE plpgsql;
