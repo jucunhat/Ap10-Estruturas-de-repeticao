5414539239bb6d28797245a201d9e385df0dce8e
Julia Cunha <juliacunhatassini@gmail.com>
2023-09-23 17:25:01 -0300

    ap10(repeticao): adicionando gerador de aleatorios

---
+++ script.sql
@@ -0,0 +1,8 @@
+-- Função de geração de aleatórios
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+INT) RETURNS INT AS
+$$
+BEGIN
+RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+END;
+$$ LANGUAGE plpgsql;
