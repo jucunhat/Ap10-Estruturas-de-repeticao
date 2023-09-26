c8e89bcecaf8fa70b21aba06eb11ecf3b545223f
Julia Cunha <juliacunhatassini@gmail.com>
2023-09-25 21:12:03 -0300

    ap10(repeticao): determinante por sarrus

--- script.sql
+++ script.sql
@@ -1,33 +1,76 @@
+-- 1.2 Faça um programa que calcule o determinante de uma matriz quadrada de ordem 3 utilizando a regra de Sarrus.
+-- Preencha a matriz com valores inteiros aleatórios no intervalo de 1 a 12.
+DO
+$$
+DECLARE
+	matriz INT[][] := ARRAY[
+					[valor_aleatorio_entre(1, 12), valor_aleatorio_entre(1, 12), valor_aleatorio_entre(1, 12)],
+					[valor_aleatorio_entre(1, 12), valor_aleatorio_entre(1, 12), valor_aleatorio_entre(1, 12)],
+					[valor_aleatorio_entre(1, 12), valor_aleatorio_entre(1, 12), valor_aleatorio_entre(1, 12)]
+					];
+	determinante FLOAT;
+	vet_aux INT[];
+	-- Declarando as posições para a visualização ficar mais fácil
+	a INT;
+	b INT;
+	c INT;
+	d INT;
+	e INT;
+	f INT;
+	g INT;
+	h INT;
+	i INT;
+
+BEGIN
+	RAISE NOTICE 'SLICE %, matriz', 1;
+	FOREACH vet_aux SLICE 1 IN ARRAY matriz LOOP
+		RAISE NOTICE '%', vet_aux;
+	END LOOP;
+
+	a = matriz[1][1];
+	b = matriz[1][2];
+	c = matriz[1][3];
+	d = matriz[2][1];
+	e = matriz[2][2];
+	f = matriz[2][3];
+	g = matriz[3][1];
+	h = matriz[3][2];
+	i = matriz[3][3];
+	determinante = (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g) - (b * d * i) - (a * f * h);
+	RAISE NOTICE 'A determinante é: %', determinante;
+END;
+$$
+
 --Leia um conjunto não determinado de pares de valores M e N 
 --(parar quando algum dos valores for menor ou igual a zero). 
 --Para cada par lido, mostre a sequência do menor até o maior e a 
 --soma dos inteiros consecutivos entre eles (incluindo o N e M).
-DO
-$$
-DECLARE
-	valor_a INT := valor_aleatorio_entre(1, 100);
-	valor_b INT := valor_aleatorio_entre(1, 100);
-	soma INT := 0;
-BEGIN
-	IF valor_a > valor_b THEN
-		RAISE NOTICE 'Valor menor: %', valor_b;
-		RAISE NOTICE 'Valor maior: %', valor_a;
-		FOR i IN valor_b..valor_a LOOP
-			RAISE NOTICE '%', i;
-			soma = soma + i;
-		END LOOP;
-		RAISE NOTICE 'soma: %', soma;
-	ELSE
-		RAISE NOTICE 'Valor menor: %', valor_a;
-		RAISE NOTICE 'Valor maior: %', valor_b;
-		FOR i IN valor_a..valor_b LOOP
-			RAISE NOTICE '%', i;
-			soma = soma + i;
-		END LOOP;
-		RAISE NOTICE 'soma: %', soma;
-	END IF;
-END;
-$$
+-- DO
+-- $$
+-- DECLARE
+-- 	valor_a INT := valor_aleatorio_entre(1, 100);
+-- 	valor_b INT := valor_aleatorio_entre(1, 100);
+-- 	soma INT := 0;
+-- BEGIN
+-- 	IF valor_a > valor_b THEN
+-- 		RAISE NOTICE 'Valor menor: %', valor_b;
+-- 		RAISE NOTICE 'Valor maior: %', valor_a;
+-- 		FOR i IN valor_b..valor_a LOOP
+-- 			RAISE NOTICE '%', i;
+-- 			soma = soma + i;
+-- 		END LOOP;
+-- 		RAISE NOTICE 'soma: %', soma;
+-- 	ELSE
+-- 		RAISE NOTICE 'Valor menor: %', valor_a;
+-- 		RAISE NOTICE 'Valor maior: %', valor_b;
+-- 		FOR i IN valor_a..valor_b LOOP
+-- 			RAISE NOTICE '%', i;
+-- 			soma = soma + i;
+-- 		END LOOP;
+-- 		RAISE NOTICE 'soma: %', soma;
+-- 	END IF;
+-- END;
+-- $$
 
 --Leia 2 valores inteiros X e Y. A seguir, calcule e mostre a soma dos 
 --números impares entre eles.
@@ -96,7 +139,7 @@ $$
 -- END;
 -- $$
 
--- -- Função de geração de aleatórios
+-- Função de geração de aleatórios
 -- CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
 -- INT) RETURNS INT AS
 -- $$
